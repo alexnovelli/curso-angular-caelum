a11b16c5b9780cb83e13f05c08f50db306b39e37
Alex Novelli <alexnovelli@gmail.com>
2019-03-23 13:21:21 -0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cmail';
+  isNewEmailFormOpen = false;
+  toggleNewEmailForm(){
+    this.isNewEmailFormOpen = !this.isNewEmailFormOpen;
+  }
 }
