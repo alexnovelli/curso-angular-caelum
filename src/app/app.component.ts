957606714e23914863a02818b6ee1a19a004fb19
Alex Novelli <alexnovelli@gmail.com>
2019-03-30 10:56:51 -0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
+import { Email } from './models/email';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  title = "Cmail";
+  
+  email: Email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  }
+
+  emailList:Email[] = [];
+
   private _isNewEmailFormOpen = false;
+
   get isNewEmailOpen(){
     return this._isNewEmailFormOpen;
   }
   toggleNewEmailForm(){
     this._isNewEmailFormOpen = !this._isNewEmailFormOpen;
   }
+  handleNewEmail(){
+    let novoEmail = new Email(this.email);
+    this.emailList.push(novoEmail);
+  }
+  
 }
